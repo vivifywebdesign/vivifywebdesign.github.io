9f0e9473cf0956670f51e1a8b4b87259ebe9e514
Jun Hyung Park <junhyungandrewpark@gmail.com>
2016-12-01 17:58:32 -0800

    initial pagge outline

---
+++ js/display.js
@@ -0,0 +1,14 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $("div.col4").css("display", "inline-block");
+    $("div.col4").jAnimateOnce('fadeInLeft'); 
+    
+    $("div.col3").jAnimateOnce('slideInDown');
+    
+    $("div.col4").mouseenter(function(){
+        
+        
+    });
+    
+});
