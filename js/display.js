6a221df6514a1a416ca00a16e2abc93e9a31fc04
Jun Hyung Park <junhyungandrewpark@gmail.com>
2016-12-02 16:14:57 -0800

    update main page

--- js/display.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {    
-    $("div.col4").css("display", "inline-block");
-    $("div.col4").jAnimateOnce('fadeInLeft');  
-    $("div.col3").jAnimateOnce('slideInDown');     
-});
